e44d06096b8f4f97a9656054de92cecfe85c0705
Artem8 <artem.girus212@gmail.com>
2025-07-21 11:57:44 +0300

    Написані моделі для товару у меню

--- root/menu/models.py
+++ root/menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    # -->
+    def __str__(self):
+        return str(self.name)
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    about = models.TextField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    photo = models.FileField()
+    price = models.FloatField()
+    
+    # --> 
+    def __str__(self):
+        return str(self.name)
