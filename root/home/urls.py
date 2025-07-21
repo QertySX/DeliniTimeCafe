d73e02e9598adc712acaacfee929be71b5ddd8c8
Artem8 <artem.girus212@gmail.com>
2025-07-21 16:09:45 +0300

    Зареєстровано Api для сортування меню

--- root/home/urls.py
+++ root/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, book
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('book_table', book, name='book_table')
 ]
