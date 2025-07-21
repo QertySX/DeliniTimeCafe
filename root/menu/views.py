b6aa3d862a45eb0ba638a1de0837e95161df4e39
Artem8 <artem.girus212@gmail.com>
2025-07-21 11:58:06 +0300

    Написана функція для відображення меню

--- root/menu/views.py
+++ root/menu/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 def menu(request):
+    product = Product.objects.all()
     return render(request, 'menu/menu.html', context={
         'title': 'Меню',
         'page': 'menu',
         'app': 'menu',
+        'product': product
     })
