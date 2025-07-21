ecd0e65b406ae387206339ccac53357ec853b535
Artem8 <artem.girus212@gmail.com>
2025-07-21 16:10:00 +0300

    Назначні зміни

--- root/home/views.py
+++ root/home/views.py
@@ -10,9 +10,16 @@ def index(request):
 
 
 def about(request):
-    '''ДОБАВИТЬ КОШИК'''
     return render(request, 'home/about.html', context={
         'title': 'Про нас ',
         'page': 'about',
         'app': 'home',
+    })
+
+
+def book(request):
+    return render(request, 'home/book_table.html', context={
+    'title': 'Бронювання',
+    'page': 'book_table',
+    'app': 'home'
     })
