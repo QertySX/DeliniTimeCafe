b3d09e9ed8fa7ec10e9074aff67b3b5282c887f9
Artem8 <artem.girus212@gmail.com>
2025-07-22 11:22:57 +0300

    Незначні зміни

--- root/menu/views.py
+++ root/menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def menu(request):
     products = Product.objects.all()
@@ -10,11 +11,12 @@ def menu(request):
     if selected_category_id:
         products = products.filter(category__id=selected_category_id)
 
+
     return render(request, 'menu/menu.html', context={
         'title': 'Меню',
         'page': 'menu',
         'app': 'menu',
-        'product': products,
+        'products': products,
         'category': category,
         'selected_category_id': selected_category_id,
     })
