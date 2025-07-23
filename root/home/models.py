62ee3ad9be5b5b1260564ff944d00cd840d1330b
Artem8 <artem.girus212@gmail.com>
2025-07-23 20:16:25 +0300

    Написана модель для брнювання столику

--- root/home/models.py
+++ root/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BookTable(models.Model):
+    name = models.CharField("Ім'я", max_length=100)
+    phone = models.CharField("Номер телефону", max_length=20)
+    email = models.EmailField("Електронна пошта", blank=True, null=True)
+    guests = models.PositiveIntegerField("Кількість гостей")
+    date = models.DateField("Дата бронювання")
+    time = models.TimeField("Час бронювання")
+    created_at = models.DateTimeField("Створено", auto_now_add=True)
+
+    def __str__(self):
+        return f"Заброньовано на {self.name} на {self.date} в {self.time}"
