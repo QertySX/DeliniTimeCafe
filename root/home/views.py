bc3fbd95d812f582399b73c4527637f6bb985902
Artem8 <artem.girus212@gmail.com>
2025-07-23 20:17:05 +0300

    Зроблене представлення для бронювання столиків

--- root/home/views.py
+++ root/home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BookTable
 
 # Create your views here.
 def index(request):
@@ -18,7 +19,24 @@ def about(request):
 
 
 def book(request):
-    return render(request, 'home/book_table.html', context={
+    if request.method == 'POST':
+        form_name = request.POST.get('name')
+        form_phone = request.POST.get('phone')
+        form_email = request.POST.get('email')
+        form_guests = request.POST.get('guests')
+        form_date = request.POST.get('date')
+        form_time = request.POST.get('time')
+
+        BookTable.objects.create(
+            name=form_name,
+            phone=form_phone,   
+            email=form_email,
+            guests=form_guests,
+            date=form_date,
+            time=form_time
+        )
+
+    return render(request, 'reservations/book_table.html', context={
     'title': 'Бронювання',
     'page': 'book_table',
     'app': 'home'
