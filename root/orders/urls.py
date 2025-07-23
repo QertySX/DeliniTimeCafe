bcd29e2b52fe6822260743f14e1a13baf5f32c26
Artem8 <artem.girus212@gmail.com>
2025-07-23 20:15:37 +0300

    Зареєстровано шляхи для роботи з кошиком

--- root/orders/urls.py
+++ root/orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import * 
+from .views import ajax_cart, reserv, ajax_cart_indicate
 
 urlpatterns = [
-    
+    path('ajax_cart', ajax_cart),
+    path('index', reserv, name='reserv'),
+    path('ajax_cart_indicate', ajax_cart_indicate),
 ]
