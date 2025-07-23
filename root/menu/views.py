dcf5614a980459bc4495c80b16d75c83e8eca91e
Artem8 <artem.girus212@gmail.com>
2025-07-23 21:44:26 +0300

    Написане представлення для відображення сторінки онлайн замовлення

--- root/menu/views.py
+++ root/menu/views.py
@@ -19,3 +19,21 @@ def menu(request):
         'category': category,
         'selected_category_id': selected_category_id,
     })
+
+
+def delivery(request):
+    products = Product.objects.all()
+    category = Category.objects.all()
+
+    selected_category_id = request.GET.get('category')
+
+    if selected_category_id:
+        products = products.filter(category__id=selected_category_id)
+    return render(request, 'menu/delivery.html', context={
+        'title': 'Меню',
+        'page': 'delivery',
+        'app': 'menu',
+        'products': products,
+        'category': category,
+        'selected_category_id': selected_category_id,
+    })
