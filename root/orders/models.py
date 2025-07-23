e9deb6ce0a2d31ce3feb31f6413a5d7226088e34
Artem8 <artem.girus212@gmail.com>
2025-07-23 20:15:52 +0300

    Написана модель для замовлень

--- root/orders/models.py
+++ root/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from menu.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    # Модель замовлення товару у кошику користувача
+    title = models.CharField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    amount = models.IntegerField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
