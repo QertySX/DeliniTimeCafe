5247452e5f6f44d90fccaced2fd3174432d0e1d1
ArtemHirus <artem.girus212@gmail.com>
2025-07-30 14:05:16 +0300

    Зареєстровано сторінки відгуків, змінення, видалення

--- root/home/urls.py
+++ root/home/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index, about, book, done, del_reserv
+from .views import index, about, book, done, del_reserv, add_reviews, del_review, edit_review
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
     path('book_table', book, name='book_table'),
     path('done', done, name='done'),
-    path('delete-reserve/<int:book_table_id>/', del_reserv, name='del_reserv')
+    path('delete-reserve/<int:book_table_id>/', del_reserv, name='del_reserv'),
+    path('add_reviews', add_reviews, name='add_reviews'),
+    path('del_review/<int:reviews_id>/', del_review, name='del_review'),
+    path('edit_review/<int:reviews_id>/', edit_review, name='edit_review')
 ]
