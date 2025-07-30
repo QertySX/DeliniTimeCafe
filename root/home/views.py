c142bd7ad83c01535a18000a0087e3cc0fdb75d6
ArtemHirus <artem.girus212@gmail.com>
2025-07-30 14:04:56 +0300

    Додано відгуки

--- root/home/views.py
+++ root/home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import BookTable
+from .models import BookTable, Reviews
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 def index(request):
+    reviews = Reviews.objects.all()
+    user_reviews = Reviews.objects.filter(user_id=request.user.id)
     return render(request, 'home/index.html', context={
         'title': 'Головна',
         'page': 'index',
         'app': 'home',
+        'reviews': reviews,
+        'user_reviews': user_reviews
     })
 
 
@@ -18,6 +23,7 @@ def about(request):
     })
 
 
+@login_required(login_url='signin')
 def book(request):
     if request.method == 'POST':
         form_name = request.POST.get('name')
@@ -59,3 +65,59 @@ def del_reserv(request, book_table_id):
     delete_reserv = BookTable.objects.get(id=book_table_id)
     delete_reserv.delete()
     return redirect('book_table')
+
+
+@login_required(login_url='signin')
+def add_reviews(request):
+    if request.method == 'GET':
+        return render(request, 'home/add_reviews.html', context={
+        'title': 'Відгуки',
+        'page': 'add_reviews',
+        'app': 'home',
+        })
+    elif request.method == 'POST':
+        try:
+            rating = int(request.POST.get('rating'))
+            text = request.POST.get('text')
+            add = Reviews.objects.create(
+                user=request.user,
+                rating=rating,
+                text=text
+            )
+            return redirect('index')
+        except Exception as e:
+            print(f'Ошибка: {e}')
+            return render(request, 'home/add_reviews.html', {
+            'error': 'Помилка при збереженні відгуку.',
+            'title': 'Відгуки',
+            'page': 'add_reviews',
+            'app': 'home',
+        })
+
+
+@login_required(login_url='signin')
+def del_review(request, reviews_id):
+    delete_review = Reviews.objects.get(id=reviews_id)
+    if request.user == delete_review.user:
+        delete_review.delete()
+        return redirect('index')
+
+
+@login_required(login_url='signin')
+def edit_review(request, reviews_id):
+    if request.method == 'GET':
+        return render(request, 'home/edit_reviews.html', context={
+            'title': 'Змінити відгук',
+            'page': 'edit_reviews',
+            'app': 'home',
+            })
+    elif request.method == 'POST':
+        edit_rating = request.POST.get('rating')
+        edit_text = request.POST.get('text')
+
+        edit_rev = Reviews.objects.get(id=reviews_id)
+        if request.user == edit_rev.user:
+            edit_rev.rating = edit_rating
+            edit_rev.text = edit_text
+            edit_rev.save()
+            return redirect('index')
