d851fe21980a5183b842873b3f922f2a26f08f13
ArtemHirus <artem.girus212@gmail.com>
2025-07-30 14:03:40 +0300

    Модель куди зберігаются замовлення після підтвердження

--- root/orders/models.py
+++ root/orders/models.py
@@ -14,3 +14,16 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+
+class OrderHistory(models.Model):
+        # Модель замовлення товару у кошику користувача
+    title = models.CharField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    amount = models.IntegerField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
